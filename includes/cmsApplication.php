90605ef1fa2e04755ea5950c86b907f70dfbe3a6
yusuffakhruddin <yusuffakhruddin4@gmail.com>
2015-03-19 09:23:04 +0700

    includes nya kok belum update

--- includes/cmsApplication.php
+++ includes/cmsApplication.php
@@ -2,6 +2,31 @@
 require_once('cmsBase.php');
 class CmsApplication extends CmsBase {
 //Anything here will be accesible in main functionality of CMS
+
+
+function run(){ //isinya adalah index.php
+	if (isset($_REQUEST['task'])) { //isset = memastikan varible sudah ada isi atau sudah dideklarasi, jika belum dideklarasi hasilnya false
+	$task = $_REQUEST['task']; //localhost/index.php?task=addcontent artinya task yang berisi addcontent
+		switch ($task) {
+			case 'addcontent':
+			$this->addcontent();
+			break;
+			case 'anyothertask':
+			$this->anyothertask();
+			break;
+			default :
+			$this->viewcontent();
+			break;
+}
+//$_REQUEST =variable php built in
+//dari deklarasi diatas yang akan direquest adalah task 
+//contoh pada localhost/index.php?task=any
+//maka task akan merequest variable yang berisi any	
+} else {
+	$this->viewcontent(); //memanggil fungsi yg berada di dalam app
+	}
+}
+
 function addcontent() {
 echo 'here add content functionality will takes palces';
 }
--- includes/templateFunctions.php
+++ includes/templateFunctions.php
@@ -3,4 +3,26 @@ require_once('cmsBase.php');
 class TemplateFunctions extends CmsBase {
 	// Semua fungsi terkait dengan
 	// pengaturan template ada di sini
+
+	var $templateName ='default'; //hanya dapat diakses di kelas templaeFunction
+	function show(){
+		require_once($this->getCurrentTemplatePath() . 'template.php');
+
+	}
+	function getCurrentTemplatePath(){
+		//untuk mendapatkan nama folder tempat menyimpan TEMPLATE
+		return 'templates/' . $this->templateName . '/';
+	}
+	function setTemplate($templateName){ //template disini adalah parameter
+		$this->templateName = $templateName;
+	}
+	//fungsi untuk menggabungkan TEMPLATE
+	//dengan management content
+	function appOutput(){
+		require_once('includes/cmsApplication.php');
+		$app = new CmsApplication();
+		$app->run();
+
+	}
 }
+?>
