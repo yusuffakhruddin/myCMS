0bc21fe0d7d989d9f4b73501f0d3dfe4a8960291
yusuffakhruddin <yusuffakhruddin4@gmail.com>
2015-03-31 12:53:48 +0700

    Update 25 maret 2015

---
+++ widgets/hello/hello.php
@@ -0,0 +1,11 @@
+<?php
+require_once('includes/cmsWidget.php');
+
+class HelloWidget extends CmsWidget{
+	function display(){
+		echo "Hello World ! Yusuf";
+	}
+
+}
+
+?>
---
+++ widgets/logo/logo.php
@@ -0,0 +1,10 @@
+<?php
+require_once('includes/cmsWidget.php');
+class LogoWidget extends CmsWidget{
+
+	function display()
+	{
+		echo 'LOGO YUSUF';
+	}
+	
+}
